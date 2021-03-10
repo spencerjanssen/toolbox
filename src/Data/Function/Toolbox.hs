2526ada1eab25e8a0ae0fd8106379bf53e44d658
Melanie Brown <brown.m@pm.me>
2021-03-10 18:55:12 -0500

    More consts

--- src/Data/Function/Toolbox.hs
+++ src/Data/Function/Toolbox.hs
@@ -52,6 +52,16 @@ module Data.Function.Toolbox (
     uncurry19,
     uncurry20,
 
+    -- * More @const@ functions
+    const2,
+    const3_1,
+    const3_2,
+    const3_3,
+    const4_1,
+    const4_2,
+    const4_3,
+    const4_4,
+
     -- * Extra utilities
     applyN,
     applyWhen,
@@ -85,6 +95,38 @@ g .: f = (g .) . f
 -- | Like @on@, but memoizes values to minimize necessary calculations.
 using :: (b -> b -> c) -> (a -> b) -> (a -> a -> c)
 cmp `using` f = \x -> let fx = f x in cmp fx . f
+
+-- | A named @(\_ x -> x)@.
+const2 :: p1 -> p2 -> p2
+const2 _ x = x
+
+-- | Three-argument 'const' that keeps the first value.
+const3_1 :: p1 -> p2 -> p3 -> p1
+const3_1 a _ _ = a
+
+-- | Three-argument 'const' that keeps the second value.
+const3_2 :: p1 -> p2 -> p3 -> p2
+const3_2 _ b _ = b
+
+-- | Three-argument 'const' that keeps the third value.
+const3_3 :: p1 -> p2 -> p3 -> p3
+const3_3 _ _ c = c
+
+-- | Four-argument 'const' that keeps the first value.
+const4_1 :: p1 -> p2 -> p3 -> p4 -> p1
+const4_1 a _ _ _ = a
+
+-- | Four-argument 'const' that keeps the second value.
+const4_2 :: p1 -> p2 -> p3 -> p4 -> p2
+const4_2 _ b _ _ = b
+
+-- | Four-argument 'const' that keeps the third value.
+const4_3 :: p1 -> p2 -> p3 -> p4 -> p3
+const4_3 _ _ c _ = c
+
+-- | Four-argument 'const' that keeps the fourth value.
+const4_4 :: p1 -> p2 -> p3 -> p4 -> p4
+const4_4 _ _ _ d = d
 
 $(mkCurryN 20)
 $(mkUncurryN 20)
